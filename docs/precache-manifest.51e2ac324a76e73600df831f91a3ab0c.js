7cd70d075a79a1df7f58b5a96b2c9eee1d694810
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-08-12 19:58:15 -0500

    Update

---
+++ docs/precache-manifest.51e2ac324a76e73600df831f91a3ab0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa1635bedae5c74d78b8850ffaf8c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9569d2ee1e10e5498968",
+    "url": "/static/js/2.3e2ab660.chunk.js"
+  },
+  {
+    "revision": "d5b445a81227876e9f4080bf0648eb92",
+    "url": "/static/js/2.3e2ab660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e92238a723d2638123",
+    "url": "/static/js/main.ca24084e.chunk.js"
+  },
+  {
+    "revision": "579c6349691bde1bcd38",
+    "url": "/static/js/runtime-main.746353a0.js"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "/static/media/TronLinkLogo.d3a8f115.png"
+  }
+]);
---
+++ docs/staking/precache-manifest.ce7daa0582adf86fd0dda7c4f6f72607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ac0c03771a9171324ecc253152394f",
+    "url": "/staking/index.html"
+  },
+  {
+    "revision": "59d3e23d53147d683efa",
+    "url": "/staking/static/js/2.f643c333.chunk.js"
+  },
+  {
+    "revision": "678226323eca686164c839dfce3432f1",
+    "url": "/staking/static/js/2.f643c333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1268f4a0e0aba85c1b",
+    "url": "/staking/static/js/main.87a1b0e8.chunk.js"
+  },
+  {
+    "revision": "410312debdd873700728",
+    "url": "/staking/static/js/runtime-main.91ca37fd.js"
+  }
+]);
