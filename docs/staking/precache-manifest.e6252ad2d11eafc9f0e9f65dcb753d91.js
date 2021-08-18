377cfc526327d1e1f71864cdb84c8812156b47ab
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-08-18 17:51:36 -0500

    update staking

---
+++ docs/staking/precache-manifest.e6252ad2d11eafc9f0e9f65dcb753d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63edff4db083604667a6b60a5cbe5290",
+    "url": "/staking/index.html"
+  },
+  {
+    "revision": "bf333e4c97fa870ee4ae",
+    "url": "/staking/static/js/2.dba84d8e.chunk.js"
+  },
+  {
+    "revision": "678226323eca686164c839dfce3432f1",
+    "url": "/staking/static/js/2.dba84d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cef684b8be87a27fc59",
+    "url": "/staking/static/js/main.608c44c1.chunk.js"
+  },
+  {
+    "revision": "410312debdd873700728",
+    "url": "/staking/static/js/runtime-main.91ca37fd.js"
+  }
+]);
