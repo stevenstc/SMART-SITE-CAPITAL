08f3fc85162a8878dffeacb7e2d9fe20355e3312
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2025-02-20 21:24:49 -0500

    actualizacion de velocidad

--- babel.config.js
+++ babel.config.js
@@ -4,10 +4,12 @@ module.exports = {
   ],
   "plugins": [
     "@babel/plugin-transform-nullish-coalescing-operator",
+    "@babel/plugin-proposal-private-methods",
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-transform-arrow-functions",
     "@babel/plugin-transform-shorthand-properties",
-    "@babel/plugin-transform-template-literals"
+    "@babel/plugin-transform-template-literals",
+    "@babel/plugin-transform-classes"
   ]
 };
